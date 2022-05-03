80e1cac35bfa94d3df63543a62b65b95664c5415
Aryan Sinha <sinha.aryan03@gmail.com>
2022-05-03 11:45:45 +0000

    ErfanGSIs: Update some tools
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>

--- update.sh
+++ update.sh
@@ -2,13 +2,14 @@
 
 LOCALDIR=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 TOOLS_DIR="$LOCALDIR/tools"
+PREB_DIR="$LOCALDIR/prebuilt"
 
 git submodule update --init --recursive
 git pull --recurse-submodules
-
+git clone https://github.com/MinatiScape/vendor_vndk "PREB_DIR"/vendor_vndk
 if [[ -d "$TOOLS_DIR/Firmware_extractor" ]]; then
     git -C "$TOOLS_DIR"/Firmware_extractor fetch origin
     git -C "$TOOLS_DIR"/Firmware_extractor reset --hard origin/master
 else
-    git clone -q https://github.com/erfanoabdi/Firmware_extractor "$TOOLS_DIR"/Firmware_extractor
+    git clone -q https://github.com/AndroidDumps/Firmware_extractor "$TOOLS_DIR"/Firmware_extractor
 fi
